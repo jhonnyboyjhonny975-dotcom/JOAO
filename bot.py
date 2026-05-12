25a5ed5a71f3ed75eb2504cebf4035b71a2cfd3e
jhonnyboyjhonny975-dotcom <jhonnyboyjhonny975@gmail.com>
2026-05-12 19:24:05 -0300

    bot.py
    Implement Discord bot with message handling and password generation.

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+import discord
+from bot_logic import gen_pass
+# A variável intents armazena as permissões do bot
+intents = discord.Intents.default()
+# Ativar a permissão para ler o conteúdo das mensagens
+intents.message_content = True
+# Criar um bot e passar as permissões
+client = discord.Client(intents=intents)
+@client.event
+async def on_ready():
+    print(f'Fizemos login como {client.user}')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
 
+gen_pass(10)  # Gerar uma senha de 10 caracteres usando a função do bot_logic.py
+
+client.run("MTUwMTYzNjc5MDMzMzk5NzE1Nw.GT2w4n.-2a3mWaHWaW_rOrDfm5SXzYtxnxRNndcitgZVk")
